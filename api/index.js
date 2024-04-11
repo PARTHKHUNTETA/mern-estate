40735d2bf2014aff507ab6f3d6bc914465b42fdf
Parth <parthkhunteta96@gmail.com>
2024-04-12 05:27:59 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,9 +1,12 @@
 
 import express from 'express';
+import connectDB from './Config/db.js';
+import dotenv from 'dotenv'
 const app = express();
 
+dotenv.config();
 
-
+connectDB()
 app.listen(3000, () => {
     console.log('listening on port 3000');
 })
