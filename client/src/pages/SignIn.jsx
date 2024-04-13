5bd448c4e3df32590f789d558e18d62f37f387aa
Parth <parthkhunteta96@gmail.com>
2024-04-14 01:20:10 +0530

    complete sign up and sign in page

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const SignIn = () => {
   return (
-    <div>SignIn</div>
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-center text-4xl font-bold my-7">Sign In</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          className="border p-3 rounded-lg"
+        ></input>
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="border p-3 rounded-lg"
+        ></input>
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover: opacity-95 disabled:opacity-80">
+          Sign In
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Don't Have  account?</p>
+        <Link to='/sign-up'><span className="text-blue-700">Sign Up</span></Link>
+      </div>
+    </div>
   )
 }
 
